749e60f336ead08463646f5d5ce4472ff71f9a6c
Le Huynh <lhuynh100@gmail.com>
2022-10-17 21:05:51 -0500

    working on index.js

--- index.js
+++ index.js
@@ -1,1 +1,80 @@
 // Your code here
+
+let createEmployeeRecord = function (row) {
+  return {
+  firstName: row[0],
+  familyName: row[1],
+  title: row[2],
+  payPerHour: row[3],
+  timeInEvents: [],
+  timeOutEvents: []
+}
+}
+let createEmployeeRecords = function(employeeRowData) {
+  return employeeRowData.map(function(row) {
+    return createEmployeeRecord(row)
+})
+}
+
+let createTimeInEvent = function(employee, dateStamp) {
+  let [date, hour] = dateStamp.split(' ')
+
+  employee.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour, 10),
+    date,
+  })
+  return employee
+}
+
+let createTimeOutEvent = function(employee, dateStamp) {
+  let [date, hour] = dateStamp.split(' ')
+
+  employee.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date,
+  })
+  return employee
+}
+
+let hoursWorkedOnDate = function(employee, findDate){
+  let inEvent = employee.timeInEvents.find(function(e){
+      return e.date === findDate
+  })
+
+  let outEvent = employee.timeOutEvents.find(function(e){
+      return e.date === findDate
+  })
+
+  return (outEvent.hour - inEvent.hour) / 100
+}
+
+let wagesEarnedOnDate = function (employee, findDate) {
+  let rawWage = hoursWorkedOnDate(employee, findDate)
+      * employee.payPerHour
+  return parseFloat(rawWage.toString())
+}
+
+let allWagesFor = function(employee) {
+let eligibleDates = employee.timeInEvents.map(function(e){
+  return e.date
+})
+
+let payable = eligibleDates.reduce(function(memo, d){
+  return memo + wagesEarnedOnDate(employee, d)
+}, 0)
+return payable
+}
+
+let findEmployeeByFirstName = function(srcArray, FirstName) {
+  return srcArray.find(function(rec){
+    return rec.firstName === firstName
+  })
+}
+
+let calculatePayroll = function(arrayOfEmployeeRecords) {
+  return arrayOfEmployeeRecords.reduce(function(memo, rec){
+    return memo + allWagesFor(rec)
+  }, 0)
+}
